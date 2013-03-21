fcc9027a4e2f618b53348386d848f1d6668d72f9
Bernhard Bender <bernhard.bender@web.de>
2013-03-21 23:19:15 +0100

    added StingIO logger
    added optional file arg to Logger
    
    Signed-off-by: Bernhard Bender <bernhard.bender@web.de>

--- src/lib/logger.py
+++ src/lib/logger.py
@@ -5,15 +5,36 @@ Created on Mar 18, 2013
 '''
 import sys
 import datetime
+import StringIO
 
 
 class Logger (object):
     
-    def __init__(self):
-        self.out = sys.stdout
+    def __init__(self, logfile=sys.stdout):
+        self.out = logfile
     
     
     def log(self, *args):
         ts = datetime.datetime.now().isoformat()
         self.out.write(ts + ': ' + ' '.join(args) + '\n')
+        
+        
+    def close(self):
+        pass
+        
+        
+
+class StringIOLogger (Logger):
+    
+    def __init__(self):
+        self.io = StringIO.StringIO()
+        super(StringIOLogger, self).__init__(self.io)
+        
+        
+    def close(self):
+        self.io.close()
+        
+        
+    def value(self):
+        return self.io.getvalue()
         
