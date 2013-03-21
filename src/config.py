1e8d1ae04fe5d32956fee1f601b978027f9e10db
Bernhard Bender <bernhard.bender@web.de>
2013-03-21 01:04:30 +0100

    config: added identification/autohrization config

--- src/config.py
+++ src/config.py
@@ -16,7 +16,7 @@ class ListenerMainGate (object):
     gate_name = 'main_gate'
     
     port_type = 'sim'
-    port_params = { 'device':'/dev/ttyS0', 'baudrate':9600, 'data':[0x06, 0x34, 0x00, 0x45, 0x8e] }
+    port_params = { 'device':'/dev/ttyS0', 'baudrate':9600, 'data':[0x06, 0x34, 0x00, 0x45, 0x8e,  0x06, 0x34, 0x00, 0x67, 0x3c] }
 
 
 listeners.append(ListenerMainGate)
@@ -35,6 +35,14 @@ class ListenerBackDoor (object):
 
 listeners.append(ListenerBackDoor)
 
+
+
+class Authorization (object):
+    
+    auth_type = 'simple'
+    
+    auth_params = { 'tokens':('em4100(06-34-00-45-8e)', ) }
+    
 
 
 class GateMainGate (object):
