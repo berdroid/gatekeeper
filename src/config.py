3272490ec2d8c3f8945829c55d6029e7df0b5149
Bernhard Bender <bernhard.bender@web.de>
2013-03-28 21:16:31 +0100

    config updates
    Signed-off-by: Bernhard Bender <bernhard.bender@web.de>

--- src/config.py
+++ src/config.py
@@ -44,8 +44,8 @@ class Authorization (object):
 @gates.append
 class GateMainGate (object):
     
-    gate_type = 'gpio'
+    gate_type = 'pulse'
     gate_name = 'main_gate'
-    gate_params = { 'hold_time': 5, 'gpio': 'gpio2' }
+    gate_params = { 'hold_time': 5, 'gpio': 'gpio2', 'pulse': 0.01 }
 
 
