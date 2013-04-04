f87409b8892c167c3f8b9d80904c3a537933af87
Bernhard Bender <bernhard.bender@web.de>
2013-04-04 23:28:39 +0200

    added SyslogMailLogger to log messages to syslog and send them via mail
    Signed-off-by: Bernhard Bender <bernhard.bender@web.de>

--- src/lib/logger.py
+++ src/lib/logger.py
@@ -7,6 +7,8 @@ import sys
 import datetime
 import StringIO
 import syslog
+import smtplib
+import email.mime.text
 
 
 class Logger (object):
@@ -54,10 +56,12 @@ class SyslogLogger (Logger):
     
     def __init__(self, logfile=None):
         super(SyslogLogger, self).__init__(logfile=logfile)
+        self.do_syslog = False
 
 
     def open_syslog(self, ident='LOG', facility=syslog.LOG_LOCAL0):
         syslog.openlog(ident=ident, facility=facility)
+        self.do_syslog = True
         
         
     def write_log(self, ts, msg):
@@ -69,6 +73,64 @@ class SyslogLogger (Logger):
 
     def close(self):
         syslog.closelog()
+        self.do_syslog = False
         
         
+
+class SyslogMailLogger (SyslogLogger):
+    
+    def __init__(self, logfile=None):
+        super(SyslogMailLogger, self).__init__(logfile)
+        self.mail_server = None
+        
+        
+    def open_maillog(self, host, port=0, username=None, password=None, body='', sender='bernhard.bender@web.de', recvr='ber.droid@googlemail.com', smtp_debug=False):
+        self.mail_server = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.body = body
+        self.sender = sender
+        self.recvr = recvr
+        self.smtp_debug = smtp_debug
+        
+        
+    def mail_log(self, ts, log_msg):
+        msg = email.mime.text.MIMEText(self.body)
+        msg['Subject'] = log_msg + ' ' + ts
+        msg['From'] = self.sender
+        msg['To'] = self.recvr
+        
+        smtp = smtplib.SMTP(self.mail_server)
+        smtp.set_debuglevel(self.smtp_debug)
+        if self.username:
+            smtp.login(self.username, self.password)
+        smtp.sendmail(self.sender, [self.recvr], msg.as_string())
+        smtp.quit()
+        
+        
+    def write_log(self, ts, msg):
+        super(SyslogMailLogger, self).write_log(ts, msg)
+        
+        if self.mail_server is not None:
+            try:
+                self.mail_log(ts, msg)
+                super(SyslogMailLogger, self).write_log(ts, 'MailLog succeded')
+            except Exception, e:
+                super(SyslogMailLogger, self).write_log(ts, 'MailLog failed with %s' % e)
+                
+            
+    def close(self):
+        super(SyslogMailLogger).close()
+        self.mail_server = None
+        
+        
+        
+if __name__ == '__main__':
+    
+    sm = SyslogMailLogger()
+    sm.open_maillog('smtp.web.de', port=465, username='***', password='***')
+    sm.log('Test from stargate')
+    
+
         
