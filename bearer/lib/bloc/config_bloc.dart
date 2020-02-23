744e4d01886e90e1a8d495a28b3fc4f35bd192f5
Bernhard Bender <bernhard.bender@web.de>
2020-02-24 00:09:15 +0100

    WiFi status is handled by the BLoC

--- bearer/lib/bloc/config_bloc.dart
+++ bearer/lib/bloc/config_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:stargate/bloc/bloc.dart';
 import 'package:stargate/stargate/udp.dart';
@@ -8,17 +9,25 @@ import 'package:stargate/stargate/udp.dart';
 class GateConfig {
   final String configJSON;
   final String username;
+  final String wifiName;
   final Map<String, dynamic> config;
 
   StarGateUDP gate;
 
   String get name => config['name'];
   String get description => config['desc'];
-  bool accessible;
+
+  bool get accessible {
+    if (config['UDP'].keys.contains('wlan'))
+      return wifiName == config['UDP']['wlan'];
+    else
+      return true;
+  }
 
   GateConfig({
     @required this.configJSON,
     @required this.username,
+    this.wifiName,
   }) : config = json.decode(configJSON) {
     gate = StarGateUDP(
       config['gate'],
@@ -37,19 +46,45 @@ class ConfigBLoC implements Bloc {
 
   final _configController = StreamController<List<GateConfig>>();
 
+  String _wifiName;
+
+  StreamSubscription _wlanStatus;
+
   Stream<List<GateConfig>> get configStream => _configController.stream;
 
   String get username => _username;
 
+  void _update() => _configController.add(_configs
+      .map((e) => GateConfig(
+            configJSON: e,
+            username: username,
+            wifiName: _wifiName,
+          ))
+      .toList());
+
   void addConfig(String config) {
     _configs.add(config);
-    _configController.add(_configs
-        .map((e) => GateConfig(configJSON: e, username: username))
-        .toList());
+    _update();
+  }
+
+  ConfigBLoC() {
+    _wlanStatus = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) async {
+      if (result == ConnectivityResult.wifi) {
+        _wifiName = await (Connectivity().getWifiName());
+        print('WiFi: $_wifiName');
+      } else {
+        _wifiName = null;
+        print('WiFi: none');
+      }
+      _update();
+    });
   }
 
   @override
   void dispose() {
     _configController.close();
+    _wlanStatus.cancel();
   }
 }
--- bearer/lib/gate.dart
+++ bearer/lib/gate.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:connectivity/connectivity.dart';
 import 'package:stargate/bloc/config_bloc.dart';
 
 import 'stargate/udp.dart';
@@ -17,8 +16,8 @@ class Gate extends StatefulWidget {
 }
 
 enum GateState {
+  idle,
   blocked,
-  accessible,
   pending,
   success,
   failed,
@@ -29,35 +28,24 @@ class _GateState extends State<Gate> {
   Map<String, dynamic> get config => widget.gateConfig.config;
   String get name => widget.gateConfig.name;
   String get description => widget.gateConfig.description;
-  GateState state = GateState.accessible;
-  StreamSubscription wlan;
+  bool get accessible => widget.gateConfig.accessible;
+
+  GateState _state = GateState.idle;
+  GateState get state => accessible ? _state : GateState.blocked;
+  set state(s) {
+    setState(() {
+      if (accessible) _state = s;
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     print('${widget.user}@${widget.gateConfig.name}');
-
-    if (config['UDP'].keys.contains('wlan')) {
-      wlan = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) async {
-        var newState = GateState.blocked;
-        print(result);
-        if (result == ConnectivityResult.wifi) {
-          var wifiName = await (Connectivity().getWifiName());
-          print(wifiName);
-          if (wifiName == config['UDP']['wlan']) {
-            newState = GateState.accessible;
-          }
-        }
-        setState(() {
-          state = newState;
-        });
-      });
-    }
   }
 
   @override
   void dispose() {
-    wlan?.cancel();
     super.dispose();
   }
 
@@ -69,7 +57,7 @@ class _GateState extends State<Gate> {
       case GateState.blocked:
         trail = Icon(Icons.block, size: 32, color: Colors.red);
         break;
-      case GateState.accessible:
+      case GateState.idle:
         trail = SizedBox(width: 32, height: 32);
         break;
       case GateState.pending:
@@ -88,23 +76,17 @@ class _GateState extends State<Gate> {
         child: Column(
           children: <Widget>[
             ListTile(
-              enabled: state == GateState.accessible,
+              enabled: state == GateState.idle,
               leading: Icon(Icons.vpn_key, size: 32),
               trailing: trail,
               title: Text(name),
               subtitle: Text(description),
               onTap: () {
-                setState(() {
-                  state = GateState.pending;
-                });
+                state = GateState.pending;
                 gate.openGate(onResult: (result) {
-                  setState(() {
-                    state = result ? GateState.success : GateState.failed;
-                  });
+                  state = result ? GateState.success : GateState.failed;
                   Future.delayed(Duration(seconds: 3)).then((_) {
-                    setState(() {
-                      state = GateState.accessible;
-                    });
+                    state = GateState.idle;
                   });
                 });
               },
