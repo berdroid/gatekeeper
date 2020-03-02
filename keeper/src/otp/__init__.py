b8f4515ae8d7b03ed51ba0a0d9fc8f9ee5348411
Bernhard Bender <bernhard.bender@web.de>
2020-03-02 01:23:41 +0100

    implemented TOTP generation and verification

--- keeper/src/otp/__init__.py
+++ keeper/src/otp/__init__.py
@@ -1,6 +1,11 @@
 import random
 import hmac
+import hashlib
 import base64
+import datetime
+import time
+import struct
+import itertools
 
 
 
@@ -13,37 +18,80 @@ class OTP (object):
         return ''.join(random.choice(cls.CHARS) for _ in range(bits/8))
     
     
-    def __init__(self, secret, digits=6, digest='SHA1'):
-        self.secret = secret,
+    def __init__(self, secret, digits=6, digest=hashlib.sha1):
+        self.secret = self._byte_secret(secret)
         self.digits = digits
         self.digest = digest
         
         
+    def _cmp_otp(self, theirs, mine):
+        theirs = (theirs + '#'*self.digits)[:self.digits]
+        c = sum(itertools.imap(lambda a, b: a==b, theirs, mine))
+        return c == self.digits
+        
+        
     def gen_otp(self, value):
-        """
-        :param value: the HMAC counter value to use as the OTP value.
-            Usually either the counter, or the computed integer based on the Unix timestamp
-        """
         if value < 0:
             raise ValueError('value must be positive integer')
-        hasher = hmac.new(self.byte_secret(), self.int_to_bytestring(value), self.digest)
-        hmac_hash = bytearray(hasher.digest())
-        offset = hmac_hash[-1] & 0xf
-        code = ((hmac_hash[offset] & 0x7f) << 24 |
-                (hmac_hash[offset + 1] & 0xff) << 16 |
-                (hmac_hash[offset + 2] & 0xff) << 8 |
-                (hmac_hash[offset + 3] & 0xff))
+        
+        hasher = hmac.new(self.secret, self._bytes64(value), self.digest)
+        hash = bytearray(hasher.digest())
+        offset = hash[-1] & 0xf
+        code = struct.unpack('>L', hash[offset:offset+4])[0]
         str_code = str(code % 10 ** self.digits)
-        while len(str_code) < self.digits:
-            str_code = '0' + str_code
-
-        return str_code
+        
+        return ('0'*self.digits + str_code)[-self.digits:]
 
 
-    def byte_secret(self):
-        missing_padding = len(self.secret) % 8
+    def _byte_secret(self, secret):
+        missing_padding = len(secret) % 8
         if missing_padding != 0:
-            self.secret += '=' * (8 - missing_padding)
-        return base64.b32decode(self.secret, casefold=True)
+            secret += '=' * (8 - missing_padding)
+        return base64.b32decode(secret, casefold=True)
 
 
+    def _bytes64(self, i, padding=8):
+        """
+        Turns an integer to the OATH specified
+        bytestring, which is fed to the HMAC
+        along with the secret
+        """
+        return struct.pack('>Q', i)
+    
+    
+
+class TOTP (OTP):
+    
+    def __init__(self, secret, digits=6, digest=hashlib.sha1, interval=30):
+        super(TOTP, self).__init__(secret, digits, digest)
+        self.interval = interval
+        
+    
+    def _at(self, point_in_time, offset=0):
+        if not isinstance(point_in_time, datetime.datetime):
+            point_in_time = datetime.datetime.fromtimestamp(int(point_in_time))
+            
+        return self.gen_otp(self._timecode(point_in_time) + offset)
+    
+    
+    def gen_totp(self):
+        return self._at(datetime.datetime.now())
+    
+    
+    def verify_totp(self, code, point_in_time=None, window=0):
+        if point_in_time is None:
+            point_in_time = datetime.datetime.now()
+
+        if not isinstance(point_in_time, datetime.datetime):
+            point_in_time = datetime.datetime.fromtimestamp(int(point_in_time))
+            
+        for offset in range(-window, window+1):
+            if self._cmp_otp(code, self._at(point_in_time, offset)):
+                return True
+            
+        return False
+    
+
+    def _timecode(self, point_in_time):
+        seconds = time.mktime(point_in_time.timetuple())
+        return int(seconds / self.interval)
