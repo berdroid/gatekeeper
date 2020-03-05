c04190708c2cf0b101f04c13c9f4a5a1483aed08
Bernhard Bender <bernhard.bender@web.de>
2020-03-06 00:45:14 +0100

    added token id independent of secret use id as default name in auth.json

--- keeper/src/mk_totp.py
+++ keeper/src/mk_totp.py
@@ -8,19 +8,22 @@ from StringIO import StringIO
 
 
 if __name__ == '__main__':
-    TEMPLATE = 'Token: {{ "name": "", "kind": "{kind}", "id": "{secret}", "blocked": false, "person_id": 0 }},'
+    TEMPLATE = 'Token: {{ "name": "{name}", "kind": "{kind}", "id": "{id}", "secret": "{secret}", "blocked": false, "person_id": 0 }},'
     
     template_name = sys.argv[1]
     
     secret = otp.OTP.gen_secret()
+    id = otp.OTP.gen_secret()[:6]
     
     data = json.load(file(template_name), encoding='utf-8')
     
     try:
         data['TOTP']['secret'] = secret
+        data['TOTP']['id'] = id
         kind = 'TOTP'
     except KeyError:
         data['COTP']['secret'] = secret
+        data['COTP']['id'] = id
         kind = 'COTP'
     
     config_string = json.dumps(data, ensure_ascii=False, encoding='utf-8')
@@ -40,7 +43,7 @@ if __name__ == '__main__':
         
         if resp['success']:
             print('Download code: {key}'.format(**resp))
-            print(TEMPLATE.format(secret=secret, kind=kind))
+            print(TEMPLATE.format(name=id, id=id, secret=secret, kind=kind))
             sys.exit(0)
             
     print(r.text)
